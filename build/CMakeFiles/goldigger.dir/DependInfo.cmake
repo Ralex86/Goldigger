6e7b45b955f778d19730a6ccda8e00052e747e17
zmijevik <persie.joseph@gmail.com>
2017-01-01 05:58:26 +0000

    Adding CMAKE for build process finding SDL libs etc

---
+++ build/CMakeFiles/goldigger.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/persie_joseph/dev/www/games/Goldigger/src/GameSDL.c" "/Users/persie_joseph/dev/www/games/Goldigger/build/CMakeFiles/goldigger.dir/src/GameSDL.c.o"
+  "/Users/persie_joseph/dev/www/games/Goldigger/src/GameState.c" "/Users/persie_joseph/dev/www/games/Goldigger/build/CMakeFiles/goldigger.dir/src/GameState.c.o"
+  "/Users/persie_joseph/dev/www/games/Goldigger/src/MapObject.c" "/Users/persie_joseph/dev/www/games/Goldigger/build/CMakeFiles/goldigger.dir/src/MapObject.c.o"
+  "/Users/persie_joseph/dev/www/games/Goldigger/src/main.c" "/Users/persie_joseph/dev/www/games/Goldigger/build/CMakeFiles/goldigger.dir/src/main.c.o"
+  "/Users/persie_joseph/dev/www/games/Goldigger/src/module.c" "/Users/persie_joseph/dev/www/games/Goldigger/build/CMakeFiles/goldigger.dir/src/module.c.o"
+  "/Users/persie_joseph/dev/www/games/Goldigger/src/player.c" "/Users/persie_joseph/dev/www/games/Goldigger/build/CMakeFiles/goldigger.dir/src/player.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/Library/Frameworks/SDL2.framework/Headers"
+  "/Library/Frameworks/SDL2_image.framework/Headers"
+  "/usr/local/include/SDL2"
+  "../goldigger"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
